a3c93592ba2584ac15c380d46fc1f12501cec204
hajder-alr <hajderalremahy@gmail.com>
2024-02-21 03:21:19 +0100

    Basic client implementation that sends a message

--- Chatt-Console/Program.cs
+++ Chatt-Console/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        connection:
+            try
+            {
+                string messageToSend;
+                TcpClient client = new TcpClient("127.0.0.1", 1302);
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    messageToSend = Console.ReadLine();
+                    if (string.IsNullOrEmpty(messageToSend))
+                        break;
+
+                    int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
+                    byte[] sendData = Encoding.ASCII.GetBytes(messageToSend);
+                    stream.Write(sendData, 0, sendData.Length);
+                }
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to connect...");
+                goto connection;
+            }
+        }
+    }
+}
